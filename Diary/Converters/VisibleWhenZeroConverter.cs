3ea1ad9738f9cf9e723f8065d7ef474a7c4d9ae2
Elias Horner <elias.horner@student.htldornbirn.at>
2020-06-26 10:48:27 +0200

    add message when there are no entries yet

---
+++ Diary/Converters/VisibleWhenZeroConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Diary.Converters {
+    class VisibleWhenZeroConverter : IValueConverter {
+        public object Convert(object v, Type t, object p, string l) =>
+        Equals(0, (int) v) ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object v, Type t, object p, string l) => null;
+    }
+}
