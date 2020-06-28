5a25f0b9341863ee88554408021e278489fffae0
Elias Horner <elias.horner@student.htldornbirn.at>
2020-06-28 19:45:33 +0200

    change font size of templates

--- Diary/Views/StyleTemplateImplementations.cs
+++ Diary/Views/StyleTemplateImplementations.cs
@@ -8,9 +8,9 @@ namespace Diary.Views {
     class StyleTemplateImplementations : List<StyleTemplate> {
         public StyleTemplateImplementations() {
             Add(new StyleTemplate("Normal", "Calibri", 13, "Black"));
-            Add(new StyleTemplate("Überschrift 1", "Calibri", 15, "#2F5496"));
-            Add(new StyleTemplate("Überschrift 2", "Calibri", 14, "#2F5496"));
-            Add(new StyleTemplate("Überschrift 3", "Calibri", 13, "#1F3763"));
+            Add(new StyleTemplate("Überschrift 1", "Calibri", 16, "#2F5496"));
+            Add(new StyleTemplate("Überschrift 2", "Calibri", 15, "#2F5496"));
+            Add(new StyleTemplate("Überschrift 3", "Calibri", 14, "#1F3763"));
             Add(new StyleTemplate("Titel", "Calibri", 20, "Black"));
 
         }
