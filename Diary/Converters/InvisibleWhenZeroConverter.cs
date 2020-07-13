3367707b84f27fbc7fe3a61bcb4db9cd3467d847
Elias Horner <elias.horner@student.htldornbirn.at>
2020-07-13 12:12:59 +0200

    InvisibleWhenZeroConverter

---
+++ Diary/Converters/InvisibleWhenZeroConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Diary.Converters {
+    class InvisibleWhenZeroConverter : IValueConverter {
+        public object Convert(object v, Type t, object p, string l) =>
+        Equals(0, (int) v) ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object v, Type t, object p, string l) => null;
+    }
+}
