039ae6cac2d7de09024e6b62a212edf3e8dbca98
Elias Horner <elias.horner@student.htldornbirn.at>
2020-11-09 15:15:53 +0100

    add debug output

--- Diary/Views/ViewEntryPage.xaml.cs
+++ Diary/Views/ViewEntryPage.xaml.cs
@@ -2,6 +2,7 @@
 using Diary.Model;
 using Diary.Services;
 using Diary.Utils;
+using Microsoft.AppCenter.Crashes;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -31,6 +32,7 @@ namespace Diary.Views {
         private AbstractPersistorService persistorService;
         private DiaryEntry entry;
         private bool wasFirstLoaded = false;
+        private List<int> debugList = new List<int>();
 
         public ViewEntryPage() {
             this.InitializeComponent();
@@ -45,7 +47,9 @@ namespace Diary.Views {
             persistorService = arg.PersistorService;
             entry = arg.Entry;
 
+            AddToDebugList(1);
             if(wasFirstLoaded && entry != null) {
+                AddToDebugList(2);
                 UpdateContent();
             }
 
@@ -56,30 +60,66 @@ namespace Diary.Views {
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e) {
+            AddToDebugList(3);
             if(!wasFirstLoaded) {
+                AddToDebugList(4);
                 // if the page wasn't unloaded in the meantime
                 if(entry != null) {
+                    AddToDebugList(5);
                     UpdateContent();
                 }
                 wasFirstLoaded = true;
             }
         }
 
+        private string Print(object var) {
+            if(var == null) return "null";
+            else return var.ToString();
+        }
+
+        private void AddToDebugList(int debug) {
+            this.debugList.Add(debug);
+            if(debugList.Count > 100) {
+                debugList.RemoveRange(0, 50);
+            }
+        }
 
         private void UpdateContent() {
-            entryButtonBarControl.DateText = entry.DateString;
-            titleElement.Text = entry.Title;
+            try {
+                entryButtonBarControl.DateText = entry.DateString;
+                titleElement.Text = entry.Title;
 
-            contentElement.IsReadOnly = false;
-            contentElement.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, entry.RtfText);
-            contentElement.Document.ApplyDisplayUpdates();
-            contentElement.IsReadOnly = true;
+                contentElement.IsReadOnly = false;
+                contentElement.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, entry.RtfText);
+                contentElement.Document.ApplyDisplayUpdates();
+                contentElement.IsReadOnly = true;
 
-            bool hasEntryImages = entry.StoredImages.Count > 0;
-            entryImagesEditor.Visibility = hasEntryImages ? Visibility.Visible : Visibility.Collapsed;
-            if(hasEntryImages) {
-                entryImagesEditor.Clear();
-                _ = entryImagesEditor.LoadImages(entry.StoredImages);
+                bool hasEntryImages = entry.StoredImages.Count > 0;
+                entryImagesEditor.Visibility = hasEntryImages ? Visibility.Visible : Visibility.Collapsed;
+                if(hasEntryImages) {
+                    entryImagesEditor.Clear();
+                    _ = entryImagesEditor.LoadImages(entry.StoredImages);
+                }
+            } catch(NullReferenceException e) {
+                var str = "";
+                foreach(int i in debugList) {
+                    str += i;
+                    str += ", ";
+                }
+
+                Crashes.TrackError(e, new Dictionary<string, string>{
+                    { "entryButtonBarControl", Print(entryButtonBarControl) },
+                    { "entry", Print(entry) },
+                    { "entry.DateString", Print(entry?.DateString) },
+                    { "entry.Title", Print(entry?.Title) },
+                    { "titleElement", Print(titleElement) },
+                    { "contentElement", Print(contentElement) },
+                    { "contentElement.Document", Print(contentElement?.Document) },
+                    { "entry.StoredImages", Print(entry?.StoredImages) },
+                    { "entryImagesEditor", Print(entryImagesEditor) },
+                    { "str", str }
+                });
+                throw;
             }
 
         }
